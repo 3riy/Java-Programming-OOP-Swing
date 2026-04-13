5819aa96f7647e796df1c46a8077d99c55cacfe3
Riya halbhavi <halbhaviriya@gmail.com>
2026-04-13 10:13:06 +0530

    Update and rename MouseClick_Event_Handling.java to Mouse_Click.java

--- Swing/Examples/Mouse Event Handling/Mouse_Click.java
+++ Swing/Examples/Mouse Event Handling/Mouse_Click.java
@@ -3,9 +3,9 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class MouseClick_Event_Handling { 
+public class Mouse_Click { 
     public static void main(String[] args) { 
-        JFrame f = new JFrame("Mouse Event Demo"); 
+        JFrame f = new JFrame("Mouse Click Event Handling"); 
         JLabel l = new JLabel("Click anywhere"); 
         l.setBounds(100, 100, 200, 30); 
         f.add(l); 
